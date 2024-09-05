fbc7f5696dd5e3a28577dd47163c12c5e06297ba
Cezary Bloch <cez.bloch@gmail.com>
2024-09-05 15:01:04 +0200

    fix packages

--- setup.py
+++ setup.py
@@ -1,15 +1,12 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='object_detection',
-    version='0.1',
+    name="object_detection",
+    version="0.1",
     packages=find_packages(),
     install_requires=[
-        # List your dependencies here
-        'torch',
-        'torchvision',
-        'Pillow',
-        'requests',
-        'pycocotools'
+        "pytest",
+        "torch",
+        "torchvision",
     ],
 )
