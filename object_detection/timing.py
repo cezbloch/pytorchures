3b1707dd9ca41989ec7df3abd90e963aaba463f0
Cezary Bloch <cez.bloch@gmail.com>
2024-09-09 19:07:59 +0200

    remove excessive log information

--- object_detection/timing.py
+++ object_detection/timing.py
@@ -31,7 +31,7 @@ class TimedLayer(torch.nn.Module):
             end_time = time.time()
             self._total_time = (end_time - start_time) * 1000
             logger.info(
-                f"{self.indent}Layer {self.layer.__class__.__name__}: {self._total_time:.6f} ms {self.layer}"
+                f"{self.indent}Layer {self.layer.__class__.__name__}: {self._total_time:.6f} ms/"
             )
             return x
 
